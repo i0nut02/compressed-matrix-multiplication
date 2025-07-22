2bc4226ba5ef2f8dea06c8188c53ae98ecd2b930
I0nut02 <ionu.radu02@gmail.com>
2025-07-22 09:37:57 +0200

    add scope

--- matmult/src/cuda/bsr_cuda.cu
+++ matmult/src/cuda/bsr_cuda.cu
@@ -63,7 +63,7 @@ void bsr_matrix_multiply_cuda(const float* A_values,
                               const int* B_rowPointers,
                               float* output,
                               int outputRows,
-                              int outputCols);
+                              int outputCols)
 {
     dim3 blockDim(TILE_SIZE, TILE_SIZE);
 
