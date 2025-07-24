459e968d540c75b38cc0871f5aeac74496cbabb2
I0nut02 <ionu.radu02@gmail.com>
2025-07-24 23:48:17 +0200

    add test mode

--- matgenerator/main.py
+++ matgenerator/main.py
@@ -3,7 +3,8 @@ from matGenerator import MatrixGenerator
 if __name__ == "__main__":
     generator = MatrixGenerator()
 
-    generator.setRows(10000).setColumns(10000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("A.inp").setPath("../matrices/").setRandomSeed(1).generate()
-    generator.setRows(20000).setColumns(20000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("B.inp").setPath("../matrices/").setRandomSeed(2).generate()
-    generator.setRows(5000).setColumns(5000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("C.inp").setPath("../matrices/").setRandomSeed(3).generate()
-    generator.setRows(1000).setColumns(1000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("D.inp").setPath("../matrices/").setRandomSeed(4).generate()
+    #generator.setRows(10000).setColumns(10000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("A.inp").setPath("../matrices/").setRandomSeed(1).generate()
+    #generator.setRows(20000).setColumns(20000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("B.inp").setPath("../matrices/").setRandomSeed(2).generate()
+    #generator.setRows(5000).setColumns(5000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("C.inp").setPath("../matrices/").setRandomSeed(3).generate()
+    #generator.setRows(1000).setColumns(1000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("D.inp").setPath("../matrices/").setRandomSeed(4).generate()
+    generator.setRows(1000).setColumns(1000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("A.inp").setPath("../matrices/").setRandomSeed(1).generate()
