63e27ecf5d831782bb8fd8bba51b12b1d4ee4fd0
I0nut02 <ionu.radu02@gmail.com>
2025-07-26 13:26:26 +0200

    add lower input

--- matgenerator/main.py
+++ matgenerator/main.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     #generator.setRows(20000).setColumns(20000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("B.inp").setPath("../matrices/").setRandomSeed(2).generate()
     #generator.setRows(5000).setColumns(5000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("C.inp").setPath("../matrices/").setRandomSeed(3).generate()
     #generator.setRows(1000).setColumns(1000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("D.inp").setPath("../matrices/").setRandomSeed(4).generate()
-    generator.setRows(1000).setColumns(1000).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("A.inp").setPath("../matrices/").setRandomSeed(1).generate()
+    generator.setRows(100).setColumns(100).setNNZPercentage(0.01).setValueRange(-100, 100).setFilename("A.inp").setPath("../matrices/").setRandomSeed(1).generate()
